41078aaf1af122278fcf6dfd7e208cb9d30de757
Vishal Bharat <vishalbharat.vs@gmail.com>
2026-05-29 14:33:29 +0530

    dfgfds

--- backend/src/main/java/com/gisma/career/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gisma.career.repository;
-
-import com.gisma.career.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
